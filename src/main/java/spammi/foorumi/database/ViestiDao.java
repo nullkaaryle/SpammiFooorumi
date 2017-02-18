0fd855e996972712ff2ee433eccdbd3b4c9c5beb
nullkaaryle <pinni@Hunajahurina.local>
2017-02-18 12:52:09 +0200

    ViestiDaon muokkaus

--- src/main/java/spammi/foorumi/database/ViestiDao.java
+++ src/main/java/spammi/foorumi/database/ViestiDao.java
@@ -1,4 +1,3 @@
-
 package spammi.foorumi.database;
 
 import java.sql.*;
@@ -9,8 +8,8 @@ import spammi.foorumi.domain.Viesti;
  *
  * @author mari
  */
-public class ViestiDao implements Dao <Viesti, Integer> {
-    
+public class ViestiDao implements Dao<Viesti, Integer> {
+
     private Database database;
     private ViestiketjuDao vkDao;
 
@@ -28,40 +27,40 @@ public class ViestiDao implements Dao <Viesti, Integer> {
     public List<Viesti> findAll() throws SQLException {
         Connection connection = database.getConnection();
         PreparedStatement stmnt = connection.prepareStatement("SELECT * FROM Viesti");
-        
+
         ResultSet rs = stmnt.executeQuery();
         List<Viesti> viestit = new ArrayList();
-        
-        while(rs.next()){
-        viestit.add(new Viesti(rs.getInt("id"),
+
+        while (rs.next()) {
+            viestit.add(new Viesti(rs.getInt("id"),
                     rs.getString("nimimerkki"),
-                    (vkDao.findOne(rs.getInt("viestiketju"))), 
+                    (vkDao.findOne(rs.getInt("viestiketju"))),
                     rs.getTimestamp("lahetysaika"),
                     rs.getString("sisalto")));
         }
-        
+
         rs.close();
         stmnt.close();
         connection.close();
-        
+
         return viestit;
     }
 
     @Override
     public Viesti create(Viesti v) throws SQLException {
         Connection connection = database.getConnection();
-        PreparedStatement stmnt = connection.prepareStatement("INSERT INTO Viesti VALUES (?, ?, ?, ?, ?)");
-        
-        stmnt.setInt(1, v.getId());
+        PreparedStatement stmnt = connection.prepareStatement("INSERT INTO Viesti (nimimerkki, viestiketju, lahetysaika, sisalto) VALUES (?, ?, DATETIME('now', 'localtime'), ?)");
+
+        //stmnt.setInt(1, v.getId());                   //ei tarvitse kun tulee automaattisesti
         stmnt.setString(2, v.getNimimerkki());
         stmnt.setInt(3, v.getViestiketju().getId());
-        stmnt.setTimestamp(4, v.getLahetysaika());
-        stmnt.setString(5, v.getSisalto());
-        
+        //stmnt.setTimestamp(4, v.getLahetysaika());    //ei tarvitse kun tulee automaattisesti
+        stmnt.setString(4, v.getSisalto());
+
         stmnt.execute();
         connection.close();
-        
+
         return new Viesti(v.getId(), v.getNimimerkki(), v.getViestiketju(), v.getLahetysaika(), v.getSisalto());
     }
-    
+
 }
