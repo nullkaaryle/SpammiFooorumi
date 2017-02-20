63349e3b2a5b788d2eda2d563201e8f8640ec01d
BikkuMyy <maripartan@gmail.com>
2017-02-20 12:31:25 +0200

    pieniä muutoksia

--- src/main/java/spammi/foorumi/database/ViestiDao.java
+++ src/main/java/spammi/foorumi/database/ViestiDao.java
@@ -50,14 +50,16 @@ public class ViestiDao implements Dao<Viesti, Integer> {
     public Viesti create(Viesti v) throws SQLException {
         Connection connection = database.getConnection();
         PreparedStatement stmnt = connection.prepareStatement("INSERT INTO Viesti (nimimerkki, viestiketju, lahetysaika, sisalto) VALUES (?, ?, DATETIME('now', 'localtime'), ?)");
+        
         stmnt.setString(1, v.getNimimerkki());
         stmnt.setInt(2, v.getViestiketju().getId());
         //stmnt.setTimestamp(4, v.getLahetysaika());    //ei tarvitse kun tulee automaattisesti
         stmnt.setString(3, v.getSisalto());
-
+        
         stmnt.execute();
         connection.close();
-
+        
+        //tietokannasta pitää saada tiedot ulos, että saa "täydellisen" Viesti-olion luotua
         return new Viesti(v.getNimimerkki(), v.getViestiketju(), v.getSisalto());
     }
 
